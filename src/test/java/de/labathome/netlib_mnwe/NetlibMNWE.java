1d7b942b2acda0c0b3ad23efc309bddc39e7a8ee
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2022-07-29 00:05:03 +0200

    Merge branch 'master' of git@github.com:jonathanschilling/netlib_mnwe.git

--- src/test/java/de/labathome/netlib_mnwe/NetlibMNWE.java
+++ src/test/java/de/labathome/netlib_mnwe/NetlibMNWE.java
@@ -5,27 +5,35 @@ import java.util.Random;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import dev.ludovic.netlib.lapack.JavaLAPACK;
 import dev.ludovic.netlib.lapack.LAPACK;
+import dev.ludovic.netlib.lapack.NativeLAPACK;
 
 public class NetlibMNWE {
 
 	@Test
 	void dlangeNullWksp() {
-		LAPACK lapack = LAPACK.getInstance();
-	
+		LAPACK lapack = NativeLAPACK.getInstance();
+//		LAPACK lapack = JavaLAPACK.getInstance();
+		
 		int n = 13;
 		double[] greensFunction = new double[n * n];
 		
+		// some predictable test data
 		Random rnd = new Random();
 		rnd.setSeed(42);
-		
 		for (int i=0; i<n*n; ++i) {
 			greensFunction[i] = rnd.nextDouble();
 		}
 		
 		final String norm = "1";
-		//double[] w = new double[0]; // works
-		double[] w = null; // does not work
+		
+		// works with both NativeLAPACK and JavaLAPACK
+//		double[] w = new double[0]; 
+		
+		// works with JavaLAPACK, does not work with NativeLAPACK
+		double[] w = null;          
+		
 		final double aNorm = lapack.dlange(norm, n, n, greensFunction, n, w);
 		
 		System.out.println(aNorm);
